fd98440f061162d52bbfad4c4d28e86e0f531f46
Muhammad Siddiqur Rahman <sidab2002@gmail.com>
2024-04-26 15:54:15 +0600

    the 2nd commit

---
+++ sum1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1,num2,sum;
+    cout<<"Enter Your Number: "<<endl;
+    cin>>num1>>num2;
+    sum = num1 + num2;
+    cout<<"The sum Is: "<<sum<<endl;
+    return 0;
+}
